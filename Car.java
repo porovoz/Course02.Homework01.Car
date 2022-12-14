1c8b2de3c566961dc2ea8aa7cf467e07da8f910d
porovoz <falkon1@list.ru>
2022-12-14 13:10:33 +0300

    Course02.Homework01.Car is done. Please check!

--- Car.java
+++ Car.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class Car {
+public class Car {
+    String brand;
+    String model;
+    double engineVolume;
+    String color;
+    int productionYear;
+    String productionCountry;
+
+    public Car(String brand, String model, int productionYear, String productionCountry, String color, double engineVolume) {
+        if (brand == null || brand.isEmpty()) {
+            this.brand = "default";
+        } else {
+            this.brand = brand;
+        }
+        if (model == null || model.isEmpty()) {
+            this.model = "default";
+        } else {
+            this.model = model;
+        }
+        if (productionYear <= 0) {
+            this.productionYear = 2000;
+        } else {
+            this.productionYear = productionYear;
+        }
+        if (productionCountry == null || productionCountry.isEmpty()) {
+            this.productionCountry = "default";
+        } else {
+            this.productionCountry = productionCountry;
+        }
+        if (color == null || color.isEmpty()) {
+            this.color = "white";
+        } else {
+            this.color = color;
+        }
+        if (engineVolume <= 0) {
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return brand + " " + model + ", year of production: " + productionYear + ", assembled in: " + productionCountry +
+                ", body color: " + color + ", engine volume - " + engineVolume + " l.";
+    }
 }
--- Main.java
+++ Main.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Car ladaGranta = new Car("Lada", "Granta", 2015, "Russia", "yellow", 1.7);
+        Car audiA8 = new Car("Audi", "A8 50L TDI quattro", 2020, "Germany", "black", 3.0);
+        Car bmwZ8 = new Car("BMW", "Z8", 2021, "Germany", "black", 3.0);
+        Car kiaSportage = new Car("Kia", "Sportage 4th generation", 2018, "South Korea", "red", 2.4);
+        Car hyundaiAvante = new Car("Hyundai", "Avante", 2016, "South Korea", "orange", 1.6);
+
+        System.out.println(ladaGranta);
+        System.out.println(audiA8);
+        System.out.println(bmwZ8);
+        System.out.println(kiaSportage);
+        System.out.println(hyundaiAvante);
+    }
 }
